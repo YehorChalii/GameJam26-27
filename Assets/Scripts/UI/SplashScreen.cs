eca235be148fd2180d8f4500ddfdf7f4f03c426a
Chaliychik <egorchaliy8@gmail.com>
2026-09-21 19:46:37 +0200

    Fixing Triggers

--- Assets/Scripts/UI/SplashScreen.cs
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SplashScreen : MonoBehaviour
 {
-    [SerializeField] private float duration;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Application.targetFrameRate = 120;
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = Application.streamingAssetsPath + "/splash.mp4";
+        videoPlayer.playOnAwake = false;
+        videoPlayer.isLooping = false;
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.Direct;
+        videoPlayer.prepareCompleted += HandleVideoPrepared;
+        videoPlayer.loopPointReached += HandleVideoFinished;
     }
 
-    private IEnumerator Start()
+    private void Start()
     {
-        yield return new WaitForSeconds(duration);
+        videoPlayer.Prepare();
+    }
 
+    private void HandleVideoPrepared(VideoPlayer videoPlayer)
+    {
+        videoPlayer.Play();
+    }
+
+    private void HandleVideoFinished(VideoPlayer videoPlayer)
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnDestroy() 
+    {
+        videoPlayer.prepareCompleted -= HandleVideoPrepared;
+        videoPlayer.loopPointReached -= HandleVideoFinished;
     }
 }
